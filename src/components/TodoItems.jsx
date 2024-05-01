845d5a96bb43c555f27f900dbab08b0c958d1266
Bhavesh Kumar <bhaveshsoni19559@gmail.com>
2024-05-01 23:02:55 +0530

    testing: working of push after the deploy

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -4,7 +4,7 @@ function TodoItems() {
   return (
     <div className="container">
       <div className="row cRow">
-        <div className="col-6">{todoName}</div>
+        <div className="col-6 text-uppercase cBold">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
           <button type="button" className="btn btn-danger cBtn">
