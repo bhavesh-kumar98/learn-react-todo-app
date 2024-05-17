3273341107e0986c6656bbbe452e0406cb2f57b8
Bhavesh Kumar <bhaveshsoni19559@gmail.com>
2024-05-17 14:41:55 +0530

    useReduser

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,56 @@ import TodoItems from "./components/TodoItems";
 import WelcomeMsg from "./components/WelcomeMsg";
 
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 import { TodoItemsContext } from "./store/todo-items-store";
 
-function App() {
-  
-  const [todoItems, setTodoItems] = useState([]);
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      { todoName: action.payload.itemName, todoDate: action.payload.itemDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter(
+      (item) => item.todoName !== action.payload.todoName
+    );
+  }
+  return newTodoItems;
+};
 
-  // const handleNewItem = (itemName, itemDate) => {
-  //     setTodoItems((currValue) => {
-  //       const newTodoItems = [
-  //         { todoName: itemName, todoDate: itemDate },
-  //         ...currValue,
-  //      ];
-  //      return newTodoItems;
-  //     });
-  // };
+function App() {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
 
   const addNewItem = (itemName, itemDate) => {
-    setTodoItems((currValue) => [
-      { todoName: itemName, todoDate: itemDate },
-      ...currValue,
-    ]);
+    const newItemsAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDate,
+      },
+    };
+    dispatchTodoItems(newItemsAction);
   };
 
-  const deleteItem = (todoItemName) => {
-    const updatedTodoItems = todoItems.filter(
-      (item) => item.todoName !== todoItemName
-    );
-    setTodoItems(updatedTodoItems);
+  const deleteItem = (todoName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
   };
 
   return (
-    <TodoItemsContext.Provider 
+    <TodoItemsContext.Provider
       value={{
         todoItems,
         addNewItem,
         deleteItem,
-        }}>
+      }}
+    >
       <center className="todo-container">
         <AppName></AppName>
         <AddTodo></AddTodo>
